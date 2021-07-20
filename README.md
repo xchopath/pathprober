8046149dffe4d8907c87f51526f11d658acca694
xchopath <novran.faathir@gmail.com>
2021-07-20 21:20:04 +0700

    Remove all commit histories and re-commit pathprober 0.3+dev

---
+++ README.md
@@ -0,0 +1,83 @@
+# pathprober
+Probe and discover HTTP pathname using brute-force methodology and filtered by specific word or 2 words at once.
+
+![version](https://img.shields.io/badge/version-0.3+dev-blue)
+[![made-with-python](https://img.shields.io/badge/Made%20with-Python-1f425f.svg)](https://www.python.org/)
+
+
+![pathprober-sample](https://raw.githubusercontent.com/xchopath/pathprober/master/sample.jpg)
+
+#### Purpose
+Brute-forcing website directories or HTTP pathname and validate using HTTP response code is not relevant anymore. This tool will help you to perform a penetration test, because it could validate the directories using specific-word or 2 words at once and the results will more accurate.
+
+#### It will help you to find:
+- Web administrator/login panel
+- Credential in some paths
+- Third-party token
+- Etc
+
+
+## Installation
+```
+git clone https://github.com/xchopath/pathprober
+cd pathprober/
+```
+
+#### Requirements
+```
+pip3 install -r requirements.txt
+```
+
+
+## Support
+- Multiple URL targets (in a file separated by newline) or single URL target
+- Multiple paths (in a file separated by newline) or single path
+- 1 word or 2 words (filter)
+- Save valid results to another file
+
+
+## Sample usage
+Multiple target, multiple path, and multiple words:
+```
+python3 pathprober.py -T target.txt -P path.txt -w "APP_NAME" -w2 "DB_PASSWORD"
+```
+
+Single target, multiple path, and single word:
+```
+python3 pathprober.py -t https://redacted.com/ -P path.txt -w "APP_NAME"
+```
+
+Multiple target, single path, multiple words, and save output to file:
+```
+python3 pathprober.py -T target.txt -p /.env -w "APP_NAME" -w2 "TWILIO" -o output.txt
+```
+
+
+## Need more help?
+```
+bash:~/pathprober$ python3 pathprober.py --help
+
+ ___  ____ ___ _  _ ___  ____ ____ ___  ____ ____
+ |__] |__|  |  |__| |__] |__/ |  | |__] |___ |__/
+ |    |  |  |  |  | |    |  \ |__| |__] |___ |  \
+       Probe HTTP pathname filtered by words
+
+usage: pathprober.py [-h] [-t https://example.com] [-p pathname] [-T target.txt] [-P path.txt] [-w Word] [-w2 Word] [-o output.txt]
+
+PathProber - Probe and discover HTTP pathname using brute-force methodology and filtered by specific word or 2 words at once
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -t https://example.com
+                        Single website target
+  -p pathname           Single pathname
+  -T target.txt         Multiple target separated by newline
+  -P path.txt           Multiple pathname separated by newline
+  -w Word               A word that you want to find in a path
+  -w2 Word              A second word that you want to find in a path
+  -o output.txt         Save the results to file
+```
+
+
+## Contributors
+- [@xchopath](https://github.com/xchopath) (from [@zerobyte-id](https://github.com/zerobyte-id))
---
+++ pathprober.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python3
+# PathProber - Probe and discover HTTP pathname using brute-force methodology and filtered by specific word or 2 words at once
+
+import datetime
+import requests
+import json
+import os
+import argparse
+import re
+import urllib3
+import sys
+from urllib.parse import urlparse
+import validators
+
+urllib3.disable_warnings()
+
+print("""
+ ___  ____ ___ _  _ ___  ____ ____ ___  ____ ____ 
+ |__] |__|  |  |__| |__] |__/ |  | |__] |___ |__/ 
+ |    |  |  |  |  | |    |  \ |__| |__] |___ |  \ 
+       Probe HTTP pathname filtered by words      
+""")
+
+### ARGS PARSER
+parser = argparse.ArgumentParser(description='PathProber - Probe and discover HTTP pathname using brute-force methodology and filtered by specific word or 2 words at once')
+parser.add_argument('-t', metavar='https://example.com', type=str, help='Single website target')
+parser.add_argument('-p', metavar='pathname', type=str, help='Single pathname')
+parser.add_argument('-T', metavar='target.txt', type=str, help='Multiple target separated by newline')
+parser.add_argument('-P', metavar='path.txt', type=str, help='Multiple pathname separated by newline')
+parser.add_argument('-w', metavar='Word', type=str, help='A word that you want to find in a path')
+parser.add_argument('-w2', metavar='Word', type=str, help='A second word that you want to find in a path')
+parser.add_argument('-o', metavar='output.txt', type=str, help='Save the results to file')
+args = parser.parse_args()
+
+### ARGS TO VARIABLE
+target = args.t
+pathname = args.p
+multi_target_file = args.T
+multi_pathname_file = args.P
+word = args.w
+word2 = args.w2
+file_output = args.o
+
+def CheckIfMatch(full_response, target_full, word, word2):
+	try:
+		if not word2 == None:
+			if word in full_response and word2 in full_response:
+				result = {'status': 'found', 'target_full': target_full, 'filter': '"' + word + '" AND "' + word2 + '"'}
+			elif word2 in full_response:
+				result = {'status': 'found', 'target_full': target_full, 'filter': '"' + word2 + '"'}
+			elif word in full_response:
+				result = {'status': 'found', 'target_full': target_full, 'filter': '"' + word + '"'}
+			else:
+				result = {'status': 'not_found', 'target_full': target_full, 'filter': None}
+		elif word in full_response:
+			result = {'status': 'found', 'target_full': target_full, 'filter': '"' + word + '"'}
+		else:
+			result = {'status': 'not_found', 'target_full': target_full, 'filter': None}
+		return result
+	except Exception as error:
+		print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+
+def ParsedTarget(target, pathname):
+	try:
+		if not target.startswith("http://") and not target.startswith("https://"):
+			target = 'http://' + target
+		else:
+			target = target
+		url_parse = urlparse(target)
+		target = url_parse.scheme + '://' + url_parse.netloc
+		target_full = re.sub('/+', '/', target + '/' + pathname)
+		target_full = re.sub(':/', '://', target_full)
+		return '{}'.format(str(target_full))
+	except Exception as error:
+		print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+
+def GetFullResponse(target_full):
+	try:
+		if validators.url(target_full):
+			response = requests.get(target_full, verify=False, allow_redirects=True)
+			full_response = []
+			for header in response.headers:
+				full_response.append(str(header) + ': ' + str(response.headers[header]))
+			full_response = '\n'.join([stings for stings in full_response])
+			full_response = full_response + '\n\n' + response.text
+			return '{}'.format(str(full_response))
+		else:
+			return ''
+	except Exception as error:
+		print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+
+def SaveToFile(output, file_output):
+	try:
+		if not file_output == None:
+			open_filename = open(file_output, 'a')
+			open_filename.write(output + '\n')
+			open_filename.close()
+	except Exception as error:
+		print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+
+### ARGS VALIDATOR
+if not target == None and not multi_target_file == None:
+	print("ERROR: -t and -T cannot be used in the same time")
+	exit()
+elif not pathname == None and not multi_pathname_file == None:
+	print("ERROR: -p and -P cannot be used in the same time")
+	exit()
+elif target == None and multi_target_file == None:
+	print("ERROR: Please add target")
+	print("HINT: Use parameter -t or -T")
+	exit()
+elif pathname == None and multi_pathname_file == None:
+	print("ERROR: Please add target")
+	print("HINT: Use parameter -p or -P")
+	exit()
+elif not multi_target_file == None and not os.path.isfile(multi_target_file):
+	print("ERROR: Could not find multi-target file")
+	exit()
+elif not multi_pathname_file == None and not os.path.isfile(multi_pathname_file):
+	print("ERROR: Could not find multi-pathname file")
+	exit()
+elif word == None:
+	print("ERROR: Please add specific word")
+	print("HINT: Use parameter -w")
+	exit()
+
+### PRINT WORD FILTERS
+if not word == None and not word2 == None:
+	print("INFO: Word filters \"{}\" and \"{}\"".format(word, word2))
+elif not word == None:
+	print("INFO: Word filter \"{}\"".format(word))
+
+### MULTIPLE TARGET AND SINGLE PATHNAME
+if not multi_target_file == None and not pathname == None:
+	try:
+		open_multi_target_file = open(multi_target_file, "r")
+		for target_row in open_multi_target_file:
+			target = target_row.split()[0]
+			try:
+				target_full = ParsedTarget(target, pathname)
+				full_response = GetFullResponse(target_full)
+				result = CheckIfMatch(full_response, target_full, word, word2)
+				if result['status'] == 'found':
+					now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+					print("FOUND! at {} - {} (filter: {})".format(now, result['target_full'], result['filter']))
+					SaveToFile(result['target_full'] + ' ({})'.format(result['filter']), file_output)
+				else:
+					print("INFO: {} not found".format(result['target_full']))
+			except Exception as error:
+				print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+		open_multi_target_file.close()
+	except Exception as error:
+		print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+
+### MULTIPLE TARGET AND MULTIPLE PATHNAME
+elif not multi_target_file == None and not multi_pathname_file == None:
+	try:
+		open_multi_target_file = open(multi_target_file, "r")
+		for target_row in open_multi_target_file:
+			target = target_row.split()[0]
+			open_multi_pathname_file = open(multi_pathname_file, "r")
+			for pathname_row in open_multi_pathname_file:
+				pathname = pathname_row.split()[0]
+				try:
+					target_full = ParsedTarget(target, pathname)
+					full_response = GetFullResponse(target_full)
+					result = CheckIfMatch(full_response, target_full, word, word2)
+					if result['status'] == 'found':
+						now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+						print("FOUND! at {} - {} (filter: {})".format(now, result['target_full'], result['filter']))
+						SaveToFile(result['target_full'] + ' ({})'.format(result['filter']), file_output)
+					else:
+						print("INFO: {} not found".format(result['target_full']))
+				except Exception as error:
+					print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+			open_multi_pathname_file.close()
+		open_multi_target_file.close()
+	except Exception as error:
+		print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+
+### SINGLE TARGET AND SINGLE PATHNAME
+elif not target == None and not pathname == None:
+	try:
+		target_full = ParsedTarget(target, pathname)
+		full_response = GetFullResponse(target_full)
+		result = CheckIfMatch(full_response, target_full, word, word2)
+		if result['status'] == 'found':
+			now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+			print("FOUND! at {} - {} (filter: {})".format(now, result['target_full'], result['filter']))
+			SaveToFile(result['target_full'] + ' ({})'.format(result['filter']), file_output)
+		else:
+			print("INFO: {} not found".format(result['target_full']))
+	except Exception as error:
+		print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+
+### SINGLE TARGET AND MULTIPLE PATHNAME
+elif not target == None and not multi_pathname_file == None:
+	try:
+		open_multi_pathname_file = open(multi_pathname_file, "r")
+		for pathname_row in open_multi_pathname_file:
+			pathname = pathname_row.split()[0]
+			try:
+				target_full = ParsedTarget(target, pathname)
+				full_response = GetFullResponse(target_full)
+				result = CheckIfMatch(full_response, target_full, word, word2)
+				if result['status'] == 'found':
+					now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+					print("FOUND! at {} - {} (filter: {})".format(now, result['target_full'], result['filter']))
+					SaveToFile(result['target_full'] + ' ({})'.format(result['filter']), file_output)
+				else:
+					print("INFO: {} not found".format(result['target_full']))
+			except Exception as error:
+				print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
+		open_multi_pathname_file.close()
+	except Exception as error:
+		print('ERROR: on line {}'.format(sys.exc_info()[-1].tb_lineno), type(error).__name__, error)
